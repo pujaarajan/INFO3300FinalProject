0548f5b058714f27b1f5e6094be71bb5f993745a
Jim Z. Li <jim-zed-li@users.noreply.github.com>
2017-05-06 15:02:33 -0400

    2016 Apple Product Releases and Dates

---
+++ releases.js
@@ -0,0 +1,87 @@
+var releases = [
+  {
+    "Time": "31-Mar-16",
+    "Model": "iPad Pro (9.7'')",
+    "Family": "iPad"
+  },
+  {
+    "Time": "31-Mar-16",
+    "Model": "iPhone SE (16 GB and 64 GB)",
+    "Family": "iPhone"
+  },
+  {
+    "Time": "31-Mar-16",
+    "Model": "iPad Pro (12.9'') (256 GB)",
+    "Family": "iPad"
+  },
+  {
+    "Time": "19-Apr-16",
+    "Model": "MacBook (Early 2016)",
+    "Family": "MacBook"
+  },
+  {
+    "Time": "16-Sep-16",
+    "Model": "iPhone 7",
+    "Family": "iPhone"
+  },
+  {
+    "Time": "16-Sep-16",
+    "Model": "iPhone 7 Plus",
+    "Family": "iPhone"
+  },
+  {
+    "Time": "16-Sep-16",
+    "Model": "iPhone 6S (32 GB)",
+    "Family": "iPhone"
+  },
+  {
+    "Time": "16-Sep-16",
+    "Model": "iPhone 6S Plus (32 GB)",
+    "Family": "iPhone"
+  },
+  {
+    "Time": "16-Sep-16",
+    "Model": "Apple Watch Series 1",
+    "Family": "Apple Watch"
+  },
+  {
+    "Time": "16-Sep-16",
+    "Model": "Apple Watch Series 2",
+    "Family": "Apple Watch"
+  },
+  {
+    "Time": "16-Sep-16",
+    "Model": "Apple Watch Hermes Series 2",
+    "Family": "Apple Watch"
+  },
+  {
+    "Time": "16-Sep-16",
+    "Model": "Apple Watch Edition Series 2",
+    "Family": "Apple Watch"
+  },
+  {
+    "Time": "27-Oct-16",
+    "Model": "MacBook Pro (4th gen) (13'')",
+    "Family": "MacBook Pro"
+  },
+  {
+    "Time": "27-Oct-16",
+    "Model": "MacBook Pro with Touch Bar (4th gen) (13'')",
+    "Family": "MacBook Pro"
+  },
+  {
+    "Time": "27-Oct-16",
+    "Model": "MacBook Pro with Touch Bar (4th gen) (15'')",
+    "Family": "MacBook Pro"
+  },
+  {
+    "Time": "28-Oct-16",
+    "Model": "Apple Watch Nike+ Series 2",
+    "Family": "Apple Watch"
+  },
+  {
+    "Time": "19-Dec-16",
+    "Model": "Apple AirPods",
+    "Family": "iPod Accessories"
+  }
+]
